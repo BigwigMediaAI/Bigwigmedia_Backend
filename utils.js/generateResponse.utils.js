dfc1c9737116d9b51467c5b6f03970742d56a2ea
automatefreely <automatefreely@gmail.com>
2024-01-27 13:24:31 +0530

    more config

--- utils.js/generateResponse.utils.js
+++ utils.js/generateResponse.utils.js
@@ -10,14 +10,17 @@ async function generateResponse(prompt) {
             {
                 role: "system",
                 content:
-                    "You are a helpful social media marketing assistant designed to generate engaging content base on user prompt.",
+                    "You are a helpful social media marketing assistant designed to generate engaging content based on user prompts and configuration settings. Tailor your tone to match the specified tone in the user prompt's configuration. Include hashtags if 'useHashTags' is true, otherwise, omit them. Integrate emojis if 'useEmoji' is true, otherwise, refrain from using emojis.",
             },
-            { role: "user", content: prompt },
+            {
+                role: "user",
+                content: prompt,
+            },
         ],
         model: "gpt-3.5-turbo-1106",
         // response_format: { type: "json_object" },
     });
-    return completion.choices[0].message.content;
+    return completion.choices[0].message.content.replace(/(\r\n|\n|\r)/gm, "");
 }
 
 module.exports = generateResponse;
