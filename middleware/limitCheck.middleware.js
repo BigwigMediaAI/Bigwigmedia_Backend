15c9f76ef41b8490343e9b68c7ed8afce8637a60
automatefreely <automatefreely@gmail.com>
2024-01-30 20:26:19 +0530

    ho

--- middleware/limitCheck.middleware.js
+++ middleware/limitCheck.middleware.js
@@ -3,6 +3,7 @@ const { response_400, response_500 } = require("../utils.js/responseCodes.utils"
 
 exports.checkLimit = async (req, res, next) => {
     try {
+        if(!req.user) return response_400(res, "Login First");
         const user = await User.findById(req.user._id);
         if (user.getLimits().current_limit <= 0) {
             return response_400(res, "Limit reached");
