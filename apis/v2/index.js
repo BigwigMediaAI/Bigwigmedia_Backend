ee2a9296fbe91f44fef010364538a1897829a880
automatefreely <automatefreely@gmail.com>
2024-02-18 13:05:29 +0530

    ok

--- apis/v2/index.js
+++ apis/v2/index.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 
 const objectsRoutes = require("./objects.v2.routes");
+const paymentRoutes = require("./payment.v2.routes");
 
 router.get("/", (req, res) => {
     res.send("API LIVE!");
 });
 
 router.use("/objects", objectsRoutes);
+router.use("/payment", paymentRoutes);
 
 
 module.exports = router;
---
+++ apis/v2/payment.v2.routes.js
@@ -0,0 +1,8 @@
+const { getPaymentm } = require("../../controllers/payment.controller");
+
+const router = require("express").Router();
+
+
+router.post("/create-checkout-session", getPaymentm);
+
+module.exports = router;
