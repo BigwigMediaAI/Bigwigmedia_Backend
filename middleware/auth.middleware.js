9e6df074b5320317c3958a09deebaff284bd9362
automatefreely <automatefreely@gmail.com>
2024-03-09 16:13:43 +0530

    hi

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -13,6 +13,18 @@ exports.auth = async (req, res, next) => {
             clerkId,
         });
         if (!user) {
+            // check if name, email and imageUrl are present and string
+            if (
+                !name ||
+                !email ||
+                !imageUrl ||
+                typeof name !== "string" ||
+                typeof email !== "string" ||
+                typeof imageUrl !== "string"
+            ) {
+                return response_401(res, "Auth failed");
+            }
+
             const reffered = req.query.reffered || null;
             user = new User({
                 clerkId,
