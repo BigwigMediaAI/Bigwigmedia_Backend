f041b1e0e701ce3fa98e77d73054ee22e7fd7fd5
gitatractivo <gitanshutalwar@gmail.com>
2024-03-09 21:24:58 +0530

    change

--- apis/v2/admin.v2.routes.js
+++ apis/v2/admin.v2.routes.js
@@ -1,9 +1,9 @@
-const { getAllUserData } = require("../../controllers/admin.controllers");
+const { getAllUserData, addCreditM } = require("../../controllers/admin.controllers");
 const { adminAuth } = require("../../middleware/adminAuth.middleware");
 
 const router = require("express").Router();
 
 router.get("/all", adminAuth, getAllUserData);
-router.post("/addCreditManual", adminAuth, )
+router.post("/addCreditManual", addCreditM)
 
 module.exports = router;
