d3dc190fed5162726088c9ecaa8766bbbf286799
gitatractivo <gitanshutalwar@gmail.com>
2024-03-09 12:53:37 +0530

    stripe integration

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,32 +1,34 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.getPaymentm = async (req, res) => {
+    console.log(req.body.product)
     const customer = await stripe.customers.create({
       metadata: {
-        userId: req.user._id,
+        userId: req.user.clerkId,
         cart: JSON.stringify(req.body.product),
       },
     });
     const { product } = req.body;
-    console.log(product)
+    // console.log(customer)
     const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [
-            {
-                price_data: {
-                    currency: "usd",
-                    product_data: {
-                        name: product.name,
-                    },
-                    unit_amount: product.price * 100,
-                },
-                quantity: product.limit,
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: product.name,
+              description: product.limit + " Credits",
             },
-        ],
-        mode: "payment",
-        customer: customer.id,
-        success_url: "https://www.bigwigmedia.ai/success",
-        cancel_url: "https://www.bigwigmedia.ai/cancel",
+            unit_amount: product.price * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      customer: customer.id,
+      success_url: "https://www.bigwigmedia.ai/success",
+      cancel_url: "https://www.bigwigmedia.ai/cancel",
     });
     
     res.json({ id: session.id });
